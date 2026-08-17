5ced4baf6266042f59b4495d883b129f3b0c80a0
Aman Dagar <amandagar454@gmail.com>
2026-08-17 17:59:25 +0530

    initialize Wantace estimator project

---
+++ server/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "Wantace API is running",
+  });
+});
+
+module.exports = app;
